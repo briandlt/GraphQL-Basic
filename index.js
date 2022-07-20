7f6e7661514954aeadc375339aa8da0c99d5799d
brian de la torre <brianesparzadlt4@gmail.com>
2022-07-19 20:17:31 -0500

    04 - Queries and Resolvers

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const { graphql, buildSchema } = require('graphql')
 // definiendo el esquema
 const schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    saludo: String
   }
 `)
 
+// Configurar los resolvers
+const resolvers = {
+  hello: () => {
+    return 'Hola Mundo'
+  },
+  saludo: () => {
+    return 'Hola a Todos'
+  }
+}
+
 // Ejecutar el query hello
-graphql({schema, source: '{ hello }'}).then((data) => {
+graphql({schema, source: '{ saludo }', rootValue: resolvers}).then((data) => {
   console.log(data)
 })
